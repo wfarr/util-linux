08858fb980be65b9329ff7ed70884566a680d446
LaMont Jones <lamont@debian.org>
2005-12-09 12:25:19 -0700

    2.12r-2 (patches unapplied)
    Imported using git-ubuntu import.

--- hwclock.sh
+++ hwclock.sh
@@ -16,72 +16,103 @@
 #		before changing this file. You risk serious clock
 #		misbehaviour otherwise.
 
+FIRST=no	# debian/rules sets this to 'yes' when creating hwclockfirst.sh
+
+# Set this to any options you might need to give to hwclock, such
+# as machine hardware clock type for Alphas.
+HWCLOCKPARS=
+
+[ ! -x /sbin/hwclock ] && exit 0
 . /etc/default/rcS
+
 . /lib/lsb/init-functions
+verbose_log_action_msg() { [ "$VERBOSE" = no ] || log_action_msg "$@"; }
+
 [ "$GMT" = "-u" ] && UTC="yes"
 case "$UTC" in
-       no|"") GMT="--localtime"	;;
-       yes)   GMT="--utc" 	;;
-       *)     log_failure_msg "Unknown UTC setting: \"$UTC\"" >&2 ;;
+       no|"")	GMT="--localtime"
+		UTC=""
+		if [ "X$FIRST" = "Xyes" ] && [ ! -r /etc/localtime ]; then
+		    if [ -z "$TZ" ]; then
+			log_action_msg "System clock was not updated at this time"
+			exit 1
+		    fi
+		fi
+		;;
+       yes)	GMT="--utc"
+		UTC="--utc"
+		;;
+       *)	log_action_msg "Unknown UTC setting: \"$UTC\""; exit 1 ;;
 esac
 
 case "$BADYEAR" in
-       no|"") BADYEAR=""	;;
-       yes)   BADYEAR="--badyear" 	;;
-       *)     log_failure_msg "Unknown BADYEAR setting: \"$BADYEAR\"" >&2 ;;
+       no|"")	BADYEAR="" ;;
+       yes)	BADYEAR="--badyear" ;;
+       *)	log_action_msg "unknown BADYEAR setting: \"$BADYEAR\""; exit 1 ;;
 esac
 
 case "$1" in
 	start)
-		if [ ! -f /etc/adjtime ]
-		then
-			echo "0.0 0 0.0" > /etc/adjtime
+		if [ ! -f /etc/adjtime ] && [ ! -e /etc/adjtime ]; then
+		    echo "0.0 0 0.0" > /etc/adjtime
 		fi
 
-		# Uncomment the hwclock --adjust line below if you want
-		# hwclock to try to correct systematic drift errors in the
-		# Hardware Clock.
-		#
-		# WARNING: If you uncomment this option, you must either make
-		# sure *nothing* changes the Hardware Clock other than
-		# hwclock --systohc, or you must delete /etc/adjtime
-		# every time someone else modifies the Hardware Clock.
-		#
-		# Common "vilains" are: ntp, MS Windows, the BIOS Setup
-		# program.
-		#
-		# WARNING: You must remember to invalidate (delete)
-		# /etc/adjtime if you ever need to set the system clock
-		# to a very different value and hwclock --adjust is being
-		# used.
-		#
-		# Please read /usr/share/doc/util-linux/README.Debian.hwclock
-		# before enablig hwclock --adjust.
-		#
-		# hwclock --adjust $GMT $BADYEAR
+		if [ "$FIRST" != yes ]; then
+		    # Uncomment the hwclock --adjust line below if you want
+		    # hwclock to try to correct systematic drift errors in the
+		    # Hardware Clock.
+		    #
+		    # WARNING: If you uncomment this option, you must either make
+		    # sure *nothing* changes the Hardware Clock other than
+		    # hwclock --systohc, or you must delete /etc/adjtime
+		    # every time someone else modifies the Hardware Clock.
+		    #
+		    # Common "vilains" are: ntp, MS Windows, the BIOS Setup
+		    # program.
+		    #
+		    # WARNING: You must remember to invalidate (delete)
+		    # /etc/adjtime if you ever need to set the system clock
+		    # to a very different value and hwclock --adjust is being
+		    # used.
+		    #
+		    # Please read /usr/share/doc/util-linux/README.Debian.hwclock
+		    # before enablig hwclock --adjust.
 
-		if [ "$HWCLOCKACCESS" != no ]
-		then
-		    log_begin_msg "Setting the System Clock using the Hardware Clock as reference..."
-		# Copies Hardware Clock time to System Clock using the correct
-		# timezone for hardware clocks in local time, and sets kernel
-		# timezone. DO NOT REMOVE.
-			/sbin/hwclock --hctosys $GMT $BADYEAR
-		#
-		#	Now that /usr/share/zoneinfo should be available,
-		#	announce the local time.
-		#
-		    if [ "$VERBOSE" != no ]
-		    then
-			log_success_msg "System Clock set. Local time: `date`"
+		    #hwclock --adjust $GMT $BADYEAR
+		    :
+		fi
+
+		if [ "$HWCLOCKACCESS" != no ]; then
+		    log_daemon_msg "Setting the system clock"
+
+		    # Copies Hardware Clock time to System Clock using the correct
+		    # timezone for hardware clocks in local time, and sets kernel
+		    # timezone. DO NOT REMOVE.
+		    /sbin/hwclock --hctosys $GMT $HWCLOCKPARS $BADYEAR
+
+		    if [ "$FIRST" = yes ]; then
+			# Copies Hardware Clock time to System Clock using the correct
+			# timezone for hardware clocks in local time, and sets kernel
+			# timezone. DO NOT REMOVE.
+			if [ -z "$TZ" ]; then
+			   /sbin/hwclock --noadjfile --hctosys $GMT $HWCLOCKPARS $BADYEAR
+			else
+			   TZ="$TZ" /sbin/hwclock --noadjfile --hctosys $GMT $HWCLOCKPARS $BADYEAR
+			fi
+
+			if /sbin/hwclock --show $GMT $HWCLOCKPARS $BADYEAR 2>&1 > /dev/null |
+			    grep -q '^The Hardware Clock registers contain values that are either invalid'; then
+				echo "Invalid system date -- setting to 1/1/2002"
+				/sbin/hwclock --set --date '1/1/2002 00:00:00' $GMT $HWCLOCKPARS $BADYEAR
+			fi
 		    fi
-		    log_end_msg 0
+
+		    #	Announce the local time.
+		    verbose_log_action_msg "System Clock set. Local time: `date $UTC`"
 		else
-		    if [ "$VERBOSE" != no ]
-		    then
-			log_warning_msg "Not setting System Clock..."
-		    fi
+		    verbose_log_action_msg "Not setting System Clock"
 		fi
+		log_end_msg 0
 		;;
 	stop|restart|reload|force-reload)
 		#
@@ -91,33 +122,27 @@ case "$1" in
 		# WARNING: If you disable this, any changes to the system
 		#          clock will not be carried across reboots.
 		#
-		if [ "$HWCLOCKACCESS" != no ]
-		then
-		    log_begin_msg "Saving the System Clock time to the Hardware Clock..."
-		    [ "$GMT" = "-u" ] && GMT="--utc"
-			/sbin/hwclock --systohc $GMT $BADYEAR
-		    if [ "$VERBOSE" != no ]
-		    then
-			log_success_msg "Hardware Clock updated to `date`."
+		if [ "$HWCLOCKACCESS" != no ]; then
+		    log_daemon_msg "Saving the system clock"
+		    if [ "$GMT" = "-u" ]; then
+			GMT="--utc"
 		    fi
+		    /sbin/hwclock --systohc $GMT $HWCLOCKPARS $BADYEAR
+		    verbose_log_action_msg "Hardware Clock updated to `date`"
 		else
-		    if [ "$VERBOSE" != no ]
-		    then
-			log_warning_msg "Not saving System Clock..."
-		    fi
+		    verbose_log_action_msg "Not saving System Clock"
 		fi
+		log_end_msg 0
 		;;
 	show)
-		if [ "$HWCLOCKACCESS" != no ]
-		then
-			/sbin/hwclock --show $GMT $BADYEAR
+		if [ "$HWCLOCKACCESS" != no ]; then
+			/sbin/hwclock --show $GMT $HWCLOCKPARS $BADYEAR
 		fi
 		;;
 	*)
-		log_success_msg "Usage: hwclock.sh {start|stop|reload|force-reload|show}" >&2
-		log_success_msg "       start sets kernel (system) clock from hardware (RTC) clock" >&2
-		log_success_msg "       stop and reload set hardware (RTC) clock from kernel (system) clock" >&2
+		log_success_msg "Usage: hwclock.sh {start|stop|reload|force-reload|show}"
+		log_success_msg "       start sets kernel (system) clock from hardware (RTC) clock"
+		log_success_msg "       stop and reload set hardware (RTC) clock from kernel (system) clock"
 		exit 1
 		;;
 esac
-
