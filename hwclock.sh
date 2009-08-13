982bd7e030ba32b5cfef6acb698ae8d9c3d82b77
LaMont Jones <lamont@debian.org>
2009-08-13 06:28:34 -0600

    2.16-3 (patches unapplied)
    Imported using git-ubuntu import.

--- hwclock.sh
+++ hwclock.sh
@@ -18,7 +18,7 @@
 
 ### BEGIN INIT INFO
 # Provides:          hwclock
-# Required-Start:    mountdevsubfs
+# Required-Start:    checkroot
 # Required-Stop:     $local_fs
 # Default-Start:     S
 # Default-Stop:      0 6
